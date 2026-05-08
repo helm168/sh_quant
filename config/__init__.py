30d87051d5a6d5ce1fff098b55324101a8775674
helm168 <sunhao_1988@msn.cn>
2026-05-08 07:20:41 +0000

    chore: re-export ROOT_DIR from config and tidy .gitignore

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,4 @@
 from .config import DATA_DIR as DATA_DIR
+from .config import ROOT_DIR as ROOT_DIR
 from .config import STUDY_RANGE_END as STUDY_RANGE_END
 from .config import STUDY_RANGE_START as STUDY_RANGE_START
